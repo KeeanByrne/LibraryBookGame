336f9d4363e9f1a332d1516edd1eeae55ba58bd4
KeeanByrne <keeanbyrne00@gmail.com>
2023-09-17 13:31:27 +0200

    Initial Commit. Worked on UI and basic functionality

---
+++ LibraryBookGame/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using LibraryBookGame.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBookGame.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+
+        public ReplacingBooksViewModel ReplacingBooksVM { get; set; }
+
+        private object _currentView;
+
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                onPropertyChanged();
+            }
+            
+        }
+
+        public MainViewModel()
+        {
+            ReplacingBooksVM = new ReplacingBooksViewModel();
+            CurrentView = ReplacingBooksVM;
+
+        }
+
+
+
+    }
+}
