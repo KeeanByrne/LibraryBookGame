3ca59de1599227e96b64e2c83da388976263d99a
KeeanByrne <keeanbyrne00@gmail.com>
2023-09-19 21:23:35 +0200

    Basic functionality working. Call numbers are generated and can be placed in a different listbox in the order the user wants to order them.

--- LibraryBookGame/MVVM/View/ReplacingBooks.xaml.cs
+++ LibraryBookGame/MVVM/View/ReplacingBooks.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,11 @@ namespace LibraryBookGame.MVVM.View
     {
 
         private List<string> callNumbers = new List<string>();
+        private List<string> draggedItems = null;
         private int score = 0;
         private ListBoxItem draggedItem = null;
         private int nextDropIndex = 0; // Keeps track of the next position to drop a call number
-
+        private Random random = new Random();
 
         public ReplacingBooks()
         {
@@ -32,19 +34,24 @@ namespace LibraryBookGame.MVVM.View
 
         private void GenerateCallNumbers()
         {
-            Random random = new Random();
+            CultureInfo cultureInfo = new CultureInfo("en-US"); //So that the call numbers are separated by a period and not by a comma (Chat-GPT)
 
             for (int i = 0; i < 10; i++)
             {
-                // Generate a random call number (e.g., "005.73 ABC")
-                string callNumber = $"{random.Next(1000, 10000) / 1000.0:F2} {GenerateRandomSurname()}";
+                // Generate a random number between 0.00 and 99.99
+                double randomNumber = random.Next(1000) / 100.0;
+
+                // Format the number with two leading zeros, two digits behind the period
+                string formattedNumber = randomNumber.ToString("0.00", cultureInfo);
+
+                // Combine the formatted number and the generated surname
+                string callNumber = $"00{formattedNumber} {GenerateRandomSurname()}";
                 callNumbers.Add(callNumber);
             }
         }
 
         private string GenerateRandomSurname()
         {
-            Random random = new Random();
             const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             return new string(Enumerable.Repeat(characters, 3).Select(s => s[random.Next(s.Length)]).ToArray());
         }
@@ -58,11 +65,32 @@ namespace LibraryBookGame.MVVM.View
             }
         }
 
+        //Method to check if the call numbers sorted by the user are correctly placed in ascending order
+
+        private bool IsSortedInAscendingOrder(ListBox listBox)
+        {
+            List<string> sortedList = new List<string>();
+            foreach (var item in listBox.Items)
+            {
+                sortedList.Add(item.ToString());
+            }
+
+            // Logic to check if sorting is in ascending order
+            for (int i = 0; i < sortedList.Count - 1; i++)
+            {
+                if (string.Compare(sortedList[i], sortedList[i + 1], StringComparison.Ordinal) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             
-
-            // Generate 10 random call numbers
+            // Method call to generate 10 random call numbers
             GenerateCallNumbers();
 
             // Display the call numbers to the user
@@ -84,7 +112,24 @@ namespace LibraryBookGame.MVVM.View
 
         private void SortButton_Click(object sender, RoutedEventArgs e)
         {
+            // Check if the call numbers are sorted in ascending order
+            bool isSorted = IsSorted(callNumbers);
 
+            // Update the score based on sorting correctness
+            score = isSorted ? 100 : 0;
+            ScoreLabel.Content = $"Score: {score}%";
+        }
+
+        private bool IsSorted(List<string> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (string.Compare(list[i], list[i + 1], StringComparison.Ordinal) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         private void RestartButton_Click(object sender, RoutedEventArgs e)
@@ -105,51 +150,80 @@ namespace LibraryBookGame.MVVM.View
 
         }
 
+        private void ListBoxItem_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                ListBoxItem listBoxItem = sender as ListBoxItem;
+
+                if (listBoxItem != null && listBoxItem.DataContext != null)
+                {
+                    draggedItem = listBoxItem;
+                    DragDrop.DoDragDrop(listBoxItem, listBoxItem.DataContext, DragDropEffects.Move);
+                }
+            }
+        }
+
         private void CallNumbersListBox_DragOver(object sender, DragEventArgs e)
         {
-
+            if (draggedItem != null)
+            {
+                e.Effects = DragDropEffects.Move;
+            }
+            e.Handled = true;
         }
 
         private void CallNumbersListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (draggedItem != null)
+            {
+                int targetIndex = CallNumbersListBox.Items.IndexOf(CallNumbersListBox.SelectedItem);
+
+                if (targetIndex >= 0)
+                {
+                    int selectedIndex = CallNumbersListBox.Items.IndexOf(draggedItem.DataContext);
+
+                    if (selectedIndex >= 0)
+                    {
+                        callNumbers.RemoveAt(selectedIndex);
+                        callNumbers.Insert(targetIndex, (string)e.Data.GetData(typeof(string)));
+                        DisplayCallNumbers();
+                    }
+                }
+
+                draggedItem = null;
+            }
+        }
+
+        //Here new text Box
+
+        private void UserSortingListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBoxItem listBoxItem)
+            {
+                DragDrop.DoDragDrop(listBoxItem, listBoxItem.DataContext, DragDropEffects.Move);
+            }
+        }
+
+        private void UserSortingListBox_Drop(object sender, DragEventArgs e)
         {
             if (sender is ListBox listBox)
             {
                 string selectedItem = (string)e.Data.GetData(typeof(string));
 
-                // Ensure that listBox.SelectedItem is not null and is a valid index
-                if (listBox.SelectedItem != null)
+                // Ensure that listBox.SelectedIndex is within a valid range
+                if (listBox.SelectedIndex < 0 || listBox.SelectedIndex >= listBox.Items.Count)
                 {
-                    int targetIndex = listBox.Items.IndexOf(listBox.SelectedItem);
-
-                    if (targetIndex >= 0)
-                    {
-                        // Insert the item at the new position
-                        listBox.Items.Insert(targetIndex, selectedItem);
-
-                        // Remove the item from its original position
-                        listBox.Items.Remove(selectedItem);
-
-                        // Update the callNumbers list to reflect the new order
-                        callNumbers.Insert(targetIndex, selectedItem);
-                        callNumbers.Remove(selectedItem);
-                    }
+                    // Insert the item at the end of the list
+                    listBox.Items.Add(selectedItem);
+                }
+                else
+                {
+                    // Insert the item at the selected index
+                    listBox.Items.Insert(listBox.SelectedIndex, selectedItem);
                 }
             }
         }
-
-        private void ListBoxItem_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void UserSortingListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
-        private void UserSortingListBox_Drop(object sender, DragEventArgs e)
-        {
-
-        }
+        
     }
 }
