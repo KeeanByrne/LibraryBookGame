f63491d3a723614c9ac20d98ad7d4cc3acf9cbcd
KeeanByrne <keeanbyrne00@gmail.com>
2023-11-19 13:16:26 +0200

    Data is being displayed in ListViews and read from DataCallNumbers.txt

--- LibraryBookGame/MVVM/View/FindingCallNumbers.xaml.cs
+++ LibraryBookGame/MVVM/View/FindingCallNumbers.xaml.cs
@@ -1,61 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO; // Add this for File class
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
 namespace LibraryBookGame.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for FindingCallNumbers.xaml
-    /// </summary>
     public partial class FindingCallNumbers : UserControl
     {
-        // Assuming you have a class-level List<string> to store the definitions
-        private List<string> definitions = new List<string>();
+        // Create a class to represent a node in the tree
+        public class TreeNode
+        {
+            public string Name { get; set; }
+            public List<TreeNode> Children { get; set; } = new List<TreeNode>();
+        }
+
+        private List<TreeNode> treeNodes = new List<TreeNode>();
+        private List<string> flattenedList = new List<string>();
 
         public FindingCallNumbers()
         {
             InitializeComponent();
 
             // Specify the path to your text file
-            string filePath = "path/to/your/textfile.txt";
+            string filePath = @"D:\Repository\LibraryBookGame\LibraryBookGame\Text\DataCallNumbers.txt";
 
-            // Call the method to read and populate the ListView
-            ReadDataFromFile(filePath);
+            // Call the method to read and populate the TreeView
+            ReadDataAndPopulateTree(filePath);
+
+            // Flatten the tree structure for display in the ListView
+            FlattenTreeForListView(treeNodes);
+
+            
         }
 
-        private void ReadDataFromFile(string filePath)
+        private void ReadDataAndPopulateTree(string filePath)
         {
             try
             {
                 // Read all lines from the file
-                definitions = File.ReadAllLines(filePath).ToList();
+                var lines = File.ReadAllLines(filePath);
 
-                // Bind the definitions list to the ListView
-                definitionListView.ItemsSource = definitions;
+                // Assuming each line in the file represents a node in the tree
+                foreach (var line in lines)
+                {
+                    // Split the line based on some delimiter (e.g., comma) if needed
+                    string[] parts = line.Split(',');
+
+                    // Create a tree node
+                    TreeNode node = new TreeNode
+                    {
+                        Name = parts[0], // Assuming the first part is the node name
+                        // Add additional properties if needed
+                    };
+
+                    // Add the node to the list
+                    treeNodes.Add(node);
+                }
             }
             catch (Exception ex)
             {
-                // Handle exceptions (e.g., file not found, etc.)
+                // Handle exceptions
                 MessageBox.Show($"Error reading file: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void FlattenTreeForListView(List<TreeNode> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                // Add the current node to the flattened list
+                flattenedList.Add(node.Name);
+
+                // Recursively add child nodes
+                FlattenTreeForListView(node.Children);
+            }
+        }
+
+        private void DisplayFilteredEntries()
+        {
+            // Clear existing items in ListViews
+            wordListView.Items.Clear();
+            definitionListView.Items.Clear();
+
+            // Filter and populate the ListViews
+            foreach (var entry in flattenedList)
+            {
+                if (entry.StartsWith("(3)"))
+                {
+                    // Entry starts with "(3)" - add to wordListView
+                    wordListView.Items.Add(entry);
+                }
+                else
+                {
+                    // Entry doesn't start with "(3)" - add to definitionListView
+                    definitionListView.Items.Add(entry);
+                }
             }
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Add your StartButton_Click logic here
+            // Call the method to display filtered entries in ListViews
+            DisplayFilteredEntries();
         }
 
         private void RestartButton_Click(object sender, RoutedEventArgs e)
         {
-            // Add your RestartButton_Click logic here
+
         }
 
         private void HowToPlayButton_Click(object sender, RoutedEventArgs e)
         {
-            // Add your HowToPlayButton_Click logic here
+
         }
+
+        
+
+
     }
 }
+
+
